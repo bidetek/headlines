8ab265d21e3e1dd744563b5871d860ec5066636a
Aritza <aritzasm@gmail.com>
2016-08-26 17:55:45 +0200

    whith Jinga Templates

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
 import feedparser
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -19,14 +20,7 @@ def get_news(publication="ctxt"):
 
 	feed = feedparser.parse(RSS_FEEDS[publication])
 	first_article = feed['entries'][0]
-	return """<html>
-		<body>
-			<h1> Titulares </h1>
-			<b>{0}</b> <br/>
-			<i>{1}</i> <br/>
-			<p>{2}</p> <br/>
-		</body>
-		</html>""".format(first_article.get("title"), first_article.get("published"),first_article.get("summary"))
+	return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
